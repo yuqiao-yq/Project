826cdf84fe633d2b2081cd21f58ce7e5b8ced1f6
zyq <1803257191@qq.com>
2020-08-31 21:02:57 +0800

    修改了注册页面

--- 品优购/js/reg.js
+++ 品优购/js/reg.js
@@ -1,18 +1,18 @@
 window.onload = function () {
     var regtel = /^1[3|4|5|7|8]\d{9}$/; // 手机号码的正则表达式
-    var regqq = /^[1-9]\d{4,}$/; // 10000
-    var regnc = /^[\u4e00-\u9fa5]{2,8}$/; // 昵称
+    // var regqq = /^[1-9]\d{4,}$/; // 10000
+    // var regnc = /^[\u4e00-\u9fa5]{2,8}$/; // 昵称
     var regmsg = /^\d{6}$/;
     var regpwd = /^[a-zA-Z0-9_-]{6,16}$/;
     var tel = document.querySelector('#tel');
-    var qq = document.querySelector('#qq');
-    var nc = document.querySelector('#nc');
+    // var qq = document.querySelector('#qq');
+    // var nc = document.querySelector('#nc');
     var msg = document.querySelector('#msg');
     var pwd = document.querySelector('#pwd');
     var surepwd = document.querySelector('#surepwd');
     regexp(tel, regtel); // 手机号码
-    regexp(qq, regqq); // qq号码
-    regexp(nc, regnc); // 昵称
+    // regexp(qq, regqq); // qq号码
+    // regexp(nc, regnc); // 昵称
     regexp(msg, regmsg); // 短信验证
     regexp(pwd, regpwd); // 密码框
     // 表单验证的函数
